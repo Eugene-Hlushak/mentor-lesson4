eb06209e5736781f9253474a5692785feff92709
IvanDemchyk <ivandem4ik@gmail.com>
2023-04-01 12:00:20 +0300

    create redux store, slice

---
+++ src/redux/sliceToDo.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const sliceToDo = createSlice({
+  name: 'todos',
+  initialState: { todos: [] },
+  reducers: {
+    addToDo: (state, action) => {
+      state.todos = [...state.todos, action.payload];
+    },
+    deleteToDo: (state, action) => {
+      state.todos.filter(todo => todo.id !== action.payload);
+    },
+  },
+});
+export const { addToDo, deleteToDo } = sliceToDo.actions;
+export default sliceToDo.reducer;
